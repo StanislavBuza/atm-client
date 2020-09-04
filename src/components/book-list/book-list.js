bf7407a613dc4bdbb0616b008af8df7aa7c10b8f
Stanislav Buza <stanislavbuza@yahoo.com>
2020-09-04 18:48:34 -0500

    sending actions to Redux Store

--- src/components/book-list/book-list.js
+++ src/components/book-list/book-list.js
@@ -1,11 +1,25 @@
 import React, {Component} from "react";
 import BookListItem from "../book-list-item";
-import { connect } from "react-redux";
+import {bindActionCreators} from "redux";
+import {connect} from "react-redux";
+
+import {withBookstoreService} from "../hoc";
+import {booksLoaded} from "../../actions";
+import {compose} from "../../utils";
 import "./book-list.css";
 
-class BookList extends Component{
-    render () {
-    const {books} = this.props;
+class BookList extends Component {
+
+    componentDidMount() {
+        //1. receive data
+        const {bookstoreService} = this.props;
+        const data = bookstoreService.getBooks();
+        this.props.booksLoaded(data)
+        //2. dispatch action to store
+    }
+
+    render() {
+        const {books} = this.props;
         return (
             <ul>
                 {
@@ -22,8 +36,18 @@ class BookList extends Component{
 
 const mapStateToProps = (state) => {
     return {
-        books : state.books
+        books: state.books
     }
 };
 
-export default connect(mapStateToProps)(BookList);
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        booksLoaded}, dispatch)
+};
+
+export default compose(
+    withBookstoreService(),
+    connect(mapStateToProps, mapDispatchToProps)
+)(BookList);
+
+// export default withBookstoreService()(connect(mapStateToProps, mapDispatchToProps)(BookList));
